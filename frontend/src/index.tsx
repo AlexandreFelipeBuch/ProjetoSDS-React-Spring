af70d7715497803275a3211446409f475ec19a5b
Felipe Buch <felipe.m.buch@gmail.com>
2021-09-13 12:40:10 -0300

    finalizando

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'assets/css/styles.css'
+import 'assets/css/styles.css';
 import App from './App';
 
 
@@ -11,3 +11,4 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
